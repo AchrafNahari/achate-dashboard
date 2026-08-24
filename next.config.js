17d8882abbfe5285f8977cfc1f679ac3a047dc56
AchrafNahari <achraf@achate.nl>
2026-08-25 00:54:40 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,28 @@
+{
+  "name": "achate-dashboard",
+  "version": "1.0.0",
+  "description": "Achaté Bol.com Seller Intelligence Dashboard",
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start",
+    "lint": "next lint"
+  },
+  "dependencies": {
+    "next": "16.3.2",
+    "react": "19.2.8",
+    "react-dom": "19.2.8",
+    "axios": "^1.6.0",
+    "recharts": "^2.10.0",
+    "tailwindcss": "^4.0.0",
+    "@tailwindcss/postcss": "^4.0.0",
+    "autoprefixer": "^10.4.0",
+    "postcss": "^8.4.0"
+  },
+  "devDependencies": {
+    "typescript": "^5.0.0",
+    "@types/react": "^19.0.0",
+    "@types/react-dom": "^19.0.0",
+    "@types/node": "^20.0.0"
+  }
+}
